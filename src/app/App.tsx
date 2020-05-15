2d1261de234bbdd39f7fecc4a6e0d926086febe4
lucassouza <ls18017@gmail.com>
2020-05-14 22:13:32 -0300

    feat(css files, propitem, listitem, public interface): many changes
    Updated css files.
    Public interface was changed.
    Background color of the assign component was
    fixed.
    Custom input file translated.
    Fixed bug in expression editor.
    Added info icon with a
    info-message prop in global css file.
    Resizer component was changed, now on mouse down the
    window.documento.body receives a prop pointEvents = none
    
    BREAKING CHANGE: Public interface was changed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,105 +4,158 @@ import { IItem, TypeValues } from './properties-editor/shared/interfaces';
 import { PropertiesEditor } from './properties-editor/PropertiesEditor';
 import './App.css';
 
-const itens: IItem[] = [
-  {
-    id: '1',
-    name: 'My name to view only',
-    isHeader: true,
-    properties: [
-      {
-        id: '0',
-        name: 'Name',
-        value: 'My name to view only',
-        type: TypeValues.viewOnly,
-        valueHasError: false,
-        nameHasError: false,
-      },
-      {
-        id: '1',
-        name: 'String',
-        value: 'Any string',
-        type: TypeValues.string,
-        editValueDisabled: false,
-        editNameDisabled: true,
-        valueHasError: true,
-      },
-      {
-        id: '2',
-        value: true,
-        name: 'Boolean',
-        type: TypeValues.boolean,
-        editValueDisabled: false,
-        editNameDisabled: true,
-        valueHasError: false,
-        nameHasError: false,
-      },
-      {
-        id: '3',
-        value: 0,
-        name: 'Number',
-        type: TypeValues.number,
-        editValueDisabled: false,
-        editNameDisabled: true,
-        valueHasError: false,
-        nameHasError: false,
-      },
-      {
-        id: '4',
-        value: null,
-        name: 'Binary',
-        type: TypeValues.binary,
-        editValueDisabled: false,
-        editNameDisabled: true,
-        valueHasError: false,
-        nameHasError: false,
-      },
-      {
-        id: '5',
-        value: '',
-        name: 'Expression',
-        type: TypeValues.expression,
-        editValueDisabled: false,
-        editNameDisabled: true,
-        valueHasError: false,
-        nameHasError: false,
-      },
-      {
-        id: '6',
-        name: 'Big string',
-        value: 'My big string',
-        type: TypeValues.bigstring,
-      },
-      {
-        id: '7',
-        value: 'true',
-        name: 'Condiction',
-        type: TypeValues.expression,
-        editValueDisabled: false,
-        editNameDisabled: true,
-        valueHasError: false,
-        nameHasError: false,
-      },
-      {
-        id: '8',
-        name: 'Name',
-        value: 'My name',
-        type: TypeValues.assign,
-        editValueDisabled: false,
-        editNameDisabled: false,
-        valueHasError: true,
-        nameHasError: false,
-      }
-    ]
-  }
-]
+const item: IItem = {
+  id: '1',
+  name: 'My name to view only',
+  isHeader: true,
+  properties: [
+    {
+      id: '0',
+      name: 'Name',
+      information: 'Minha infromação de teste. Esta informação serve para informar algo que pode ser importante ao usuário!',
+      value: 'My name to view only',
+      type: TypeValues.viewOnly,
+      valueHasError: false,
+      nameHasError: false,
+    },
+    {
+      id: '1',
+      name: 'String',
+      value: 'Any string',
+      type: TypeValues.string,
+      information: 'Minha infromação de teste',
+      editValueDisabled: false,
+      editNameDisabled: false,
+      valueHasError: false,
+    },
+    {
+      id: '2',
+      value: true,
+      name: 'Boolean',
+      information: 'Minha infromação de teste',
+      type: TypeValues.boolean,
+      editValueDisabled: false,
+      editNameDisabled: true,
+      valueHasError: false,
+      nameHasError: false,
+    },
+    {
+      id: '3',
+      value: 0,
+      name: 'Number',
+      information: 'Minha infromação de teste',
+      type: TypeValues.number,
+      editValueDisabled: false,
+      editNameDisabled: true,
+      valueHasError: false,
+      nameHasError: false,
+    },
+    {
+      id: '4',
+      value: null,
+      name: 'Binary',
+      information: 'Minha infromação de teste',
+      type: TypeValues.binary,
+      editValueDisabled: false,
+      editNameDisabled: true,
+      valueHasError: false,
+      nameHasError: false,
+    },
+    {
+      id: '5',
+      value: '',
+      name: 'Expression',
+      information: 'Minha infromação de teste',
+      type: TypeValues.expression,
+      editValueDisabled: false,
+      editNameDisabled: true,
+      valueHasError: false,
+      nameHasError: false,
+    },
+    {
+      id: '6',
+      name: 'Big string',
+      value: 'My big string',
+      type: TypeValues.bigstring,
+      information: 'Minha infromação de teste',
+    },
+    {
+      id: '7',
+      value: 'true',
+      name: 'Condiction',
+      information: 'Minha infromação de teste',
+      type: TypeValues.expression,
+      editValueDisabled: false,
+      editNameDisabled: true,
+      valueHasError: false,
+      nameHasError: false,
+    },
+    {
+      id: '8',
+      name: 'Name',
+      value: 'My name',
+      information: 'Minha infromação de teste',
+      type: TypeValues.assign,
+      editValueDisabled: false,
+      editNameDisabled: false,
+      valueHasError: false,
+      nameHasError: false,
+    },
+    {
+      id: '9',
+      value: 'opts',
+      editValueDisabled: false,
+      name: 'Combo',
+      type: TypeValues.selection,
+      information: 'Combo box para seleção única de itens',
+      suggestions: [
+        {
+          label: 'Opção sugerida',
+          description: '',
+          disabled: false,
+          value: 'opts1',
+          name: 'opts1',
+        },
+        {
+          label: 'Opção sugerida 1',
+          description: '',
+          disabled: false,
+          value: 'opts2',
+          name: 'opts2',
+        },
+        {
+          label: 'Opção sugerida 2',
+          description: '',
+          disabled: false,
+          value: 'opts3',
+          name: 'opts3',
+        },
+        {
+          label: 'Opção sugerida disable',
+          description: '',
+          disabled: true,
+          value: 'opts4',
+          name: 'opts4',
+        }
+      ]
+    },
+    {
+      id: '10',
+      name: 'Combo Yes/No',
+      value: 'false',
+      editValueDisabled: false,
+      type: TypeValues.yesNoSelection,
+      information: 'Combo box para seleção de sim ou não(boolean)'
+    }
+  ]
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div style={{ maxWidth: 300, minWidth: 300, alignContent: '' }}>
         <PropertiesEditor
-          itens={itens}
+          item={item}
           onChange={itens => console.log(itens)}
           onChangeInputWidth={(width) => console.log(width)}
         />
