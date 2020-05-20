9dfca9bf63e1ac9bdf8f12fab96532ba3c28d56e
lucassouza <ls18017@gmail.com>
2020-05-20 18:46:44 -0300

    fix: improvements

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -167,7 +167,7 @@ const item: IItem = {
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div style={{ maxWidth: 300, minWidth: 300, alignContent: '' }}>
+      <div style={{ maxWidth: 300, minWidth: 300, alignContent: '', boxShadow: 'black 0px 0px 14px 0px' }}>
         <PropertiesEditor
           item={item}
           onChange={itens => console.log(itens)}
